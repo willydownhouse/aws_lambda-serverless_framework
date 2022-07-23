2272629aa3f6d45d2dbbce2e221ecc7bce17c9ac
willydownhouse <ville.l.alatalo@gmail.com>
2022-07-23 14:44:12 +0200

    file_upload

---
+++ src/photoUpload.js
@@ -0,0 +1,32 @@
+const AWS = require("aws-sdk");
+
+const { buildErrorObject } = require("../utils/appError");
+const { extractFile } = require("../utils/parseFile");
+
+const BUCKET = process.env.BUCKET;
+
+const s3 = new AWS.S3();
+
+module.exports.photoUpload = async (event) => {
+  try {
+    const { body } = await extractFile(event);
+
+    await s3
+      .putObject({
+        Bucket: BUCKET,
+        Key: body.fileInfo.filename,
+        ACL: "public-read",
+        Body: body.file,
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        url: `https://${BUCKET}.s3.amazonaws.com/${body.fileInfo.filename}`,
+      }),
+    };
+  } catch (err) {
+    return buildErrorObject(err);
+  }
+};
---
+++ utils/parseFile.js
@@ -0,0 +1,49 @@
+const busboy = require("busboy");
+
+const getContentType = (event) => {
+  const contentType = event.headers["content-type"];
+  if (!contentType) {
+    return event.headers["Content-Type"];
+  }
+  return contentType;
+};
+
+const extractFile = (event) =>
+  new Promise((resolve, reject) => {
+    const bb = busboy({
+      headers: {
+        "content-type": getContentType(event),
+      },
+    });
+
+    let result = {
+      file: "",
+      fileInfo: "",
+      //contentType: "",
+    };
+
+    bb.on("file", (fieldname, file, fileData, encoding, mimetype) => {
+      file.on("data", (data) => {
+        result.file = data;
+      });
+
+      file.on("end", () => {
+        result.fileInfo = fileData;
+      });
+    });
+
+    // bb.on("field", (fieldname, value) => {
+    //   result[fieldname] = value;
+    // });
+
+    bb.on("error", (error) => reject(error));
+    bb.on("finish", () => {
+      event.body = result;
+      resolve(event);
+    });
+
+    bb.write(event.body, event.isBase64Encoded ? "base64" : "binary");
+    bb.end();
+  });
+
+module.exports = { extractFile };
